77c74adc34ce2ce16d39db3194158637bda30428
vinayk02 <80611746+vinayk02@users.noreply.github.com>
2022-02-03 11:27:59 -0500

    display points

--- triangle.java
+++ triangle.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.io.*;
 import java.awt.Color;
-public class Triangle {
+public class triangle {
  
     public static void main(String[] args) {
         int startX = 0;
@@ -12,11 +12,13 @@ public class Triangle {
         int cols = 0;
         BufferedReader reader;
         String temp = "";
-		try {
+
+        try {
 			reader = new BufferedReader(new FileReader(
 					"jatt.txt"));
 			String line = reader.readLine();
             int lineCounter = 0;
+            
 			while (line != null) {
                 if(lineCounter == 0){
                     temp = line;
@@ -38,6 +40,7 @@ public class Triangle {
                
                 lineCounter++;
 			}
+            
 			reader.close();
 		} catch (IOException e) {
             e.printStackTrace();
@@ -59,6 +62,58 @@ public class Triangle {
         StdDraw.setPenRadius(0.1);
         StdDraw.point(startX, (cols + 2) - startY ) ;
         StdDraw.point(goalX, (cols + 2) - goalY ) ;
-       
+       int [][] openData = new int[rows][cols];
+        int[] dataScanner = new int[rows * cols];
+        try {
+			reader = new BufferedReader(new FileReader(
+					"jatt.txt"));
+			String line = reader.readLine();
+            int lineCounter = 0;
+            int openOrNot = 0; 
+            int count = 0;
+			while (line != null) {
+                if(lineCounter > 2){
+                    temp = line;
+                    
+                    openOrNot = Integer.parseInt(temp.substring(4 , 5));
+                   dataScanner[count] = openOrNot;
+                 count++;
+                }
+				// read next line
+				line = reader.readLine();
+               
+                lineCounter++;
+			}
+			reader.close();
+		} catch (IOException e) {
+            e.printStackTrace();
+		}
+        int count = 0;
+          
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                openData[i][j] = dataScanner[count++];
+            }
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(openData[i][j] == 1){
+                    int xNoGo = i ;
+                    int yNoGo = j;
+                    System.out.println(i);
+                    System.out.println(j);
+                    StdDraw.setPenRadius(0.01);
+                    StdDraw.setPenColor(255, 0, 0);
+                    StdDraw.point(i + 1, (cols + 1) - j ) ;
+                    StdDraw.line(i + 1, (cols + 1) - j , i + 1, (cols) - j );
+                    StdDraw.line(i + 1, (cols + 1) - j , i + 2, (cols + 1) - j );
+                    StdDraw.line(i + 2, (cols + 1) - j , i + 2, (cols) - j );
+                    StdDraw.line(i + 1, (cols ) - j , i + 2, (cols) - j );
+                    
+                }
+              
+            }
+        }
+
     }
 }
