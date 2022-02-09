5af4f097a90d624c909e985a34fb4f70388b1cf2
vinayk02 <80611746+vinayk02@users.noreply.github.com>
2022-02-09 14:52:10 -0500

    main

--- thetastar.java
+++ thetastar.java
@@ -1,16 +1,22 @@
 import java.util.Scanner;
+
+import javax.swing.text.html.HTMLDocument.RunElement;
+
 import java.io.*;
 import java.nio.file.FileAlreadyExistsException;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+public class thetastar {
 
-public class triangle {
+
+
     public static class Point {
         public int x;
         public int y;
         public Point previous;
+        
 
         public Point(int x, int y, Point previous) {
             this.x = x;
@@ -32,32 +38,75 @@ public class triangle {
 
         public Point offset(int ox, int oy) { return new Point(x + ox, y + oy, this);  }
     }
-    public static boolean IsWalkable(int [][ ] grid, Point point, String operation, Point originalPoint){
-        
-        if (point.y < 0 || point.y > grid[0].length - 1 ){
-            return false;
+    public static boolean IsWalkable(int [][ ] grid, Point point, String operation, Point originalPoint, Point start, Point end){
+        int x0 = originalPoint.x;
+        int y0 = originalPoint.y;
+        int x1 = point.x;
+        int y1 = point.y;
+        double dX = x1- x0;
+        double dY = y1 - y0;
+        double f = 0;
+       
+        if(dY <  0){
+            dY = Math.abs(dY);
+            originalPoint.y = 0;
+
+        }else{
+            originalPoint.y = 1;
+        } 
+        if(dX <  0){
+            dX = Math.abs(dX);
+            originalPoint.x = 0;
+
+        }else{
+            originalPoint.x = 1;
         }
-        else if(point.x < 0 || point.x > grid.length - 1){
-            return false;
-        }
-            else if(operation == "upRight" || operation == "upLeft" || operation == "downRight" || operation =="downLeft"){
-                if(grid[originalPoint.x][originalPoint.y] == 1){
+        if(dX >= dY){
+            while(x0 != x1){
+                f = f + dY;
+                if(f >= dX){
+                    if(grid[x0+ ((originalPoint.x - 1)/2)][y0 +((originalPoint.y - 1)/2)] == 1){
+                        return false;
+                    }
+                   y0 = y0 + originalPoint.y; 
+                }
+                if(f != 0  && grid[x0+ ((originalPoint.x - 1)/2)][y0 +((originalPoint.y - 1)/2)] == 1){
                     return false;
                 }
-                else{
-                    return grid[point.x][point.y] == 0;
+                if(dY == 0 && grid[x0 + ((originalPoint.x - 1)/ 2)][y0] == 1 && grid[x0 + ((originalPoint.x - 1)/ 2)][y0 - 1] == 1){
+                    return false;
                 }
+                x0 = x0 + originalPoint.x;
             }
-            
-        else{
+        }else{
+            while(y0 != y1){
+                f = f +dX;
+                if(f >= dY){
 
-            return grid[point.x][point.y] == 0;
-        } 
+                    if(grid[x0 + ((originalPoint.x - 1)/ 2)][y0 + ((originalPoint.y - 1) / 2)] == 1){
+                        return false;
+                    }
+                    x0 = x0 + originalPoint.x;
+                    f = f - dY;
+                }
+                if(f != 0  && grid[x0+ ((originalPoint.x - 1)/2)][y0 +((originalPoint.y - 1)/2)] == 1){
+                    return false;
+                }
+                if(dX == 0 && grid[x0][y0 + ((originalPoint.y - 1)/ 2)] == 1 && grid[x0 - 1][y0 + ((originalPoint.y - 1) / 2)] == 1){
+                    return false;
+                }
+                y0 = y0 + originalPoint.y;
+
+            }
+        }
+        return grid[point.x][point.y];
+        
+      
         
         
     }
 
-    public static List<Point> FindNeighbors(int[][] map, Point point) {
+    public static List<Point> FindNeighbors(int[][] map, Point point, Point start, Point end) {
         List<Point> neighbors = new ArrayList<>();
         Point up = point.offset(0,  1);
         Point upRight = point.offset(1,  1);
@@ -69,14 +118,14 @@ public class triangle {
         Point right = point.offset(1, 0);
        
         
-        if (IsWalkable(map, up, "up", point)) neighbors.add(up);
-        if (IsWalkable(map, down, "down", point)) neighbors.add(down);
-        if (IsWalkable(map, left, "left", point)) neighbors.add(left);
-        if (IsWalkable(map, right, "right", point)) neighbors.add(right);
-        if (IsWalkable(map, upRight, "upRight", point))neighbors.add(upRight);
-        if (IsWalkable(map, upLeft, "upLeft", point)) neighbors.add(upLeft);
-        if (IsWalkable(map, downRight, "downRight", point)) neighbors.add(downRight);
-        if (IsWalkable(map, downLeft, "downLeft", point)) neighbors.add(downLeft);
+        if (IsWalkable(map, up, "up", point, start , end)) neighbors.add(up);
+        if (IsWalkable(map, down, "down", point, start , end)) neighbors.add(down);
+        if (IsWalkable(map, left, "left", point, start , end)) neighbors.add(left);
+        if (IsWalkable(map, right, "right", point, start , end)) neighbors.add(right);
+        if (IsWalkable(map, upRight, "upRight", point, start , end))neighbors.add(upRight);
+        if (IsWalkable(map, upLeft, "upLeft", point, start , end)) neighbors.add(upLeft);
+        if (IsWalkable(map, downRight, "downRight", point, start , end)) neighbors.add(downRight);
+        if (IsWalkable(map, downLeft, "downLeft", point, start , end)) neighbors.add(downLeft);
         return neighbors;
     }
 
@@ -88,7 +137,7 @@ public class triangle {
             List<Point> newOpen = new ArrayList<>();
             for(int i = 0; i < used.size(); ++i){
                 Point point = used.get(i);
-                for (Point neighbor : FindNeighbors(map, point)) {
+                for (Point neighbor : FindNeighbors(map, point, start, end)) {
                    
                     if (!used.contains(neighbor) && !newOpen.contains(neighbor)) {
                         newOpen.add(neighbor);
@@ -298,5 +347,6 @@ public class triangle {
         }
 
 
-    }
 
+
+}
